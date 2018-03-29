99b4224a10da6919fbf639d91c3b3773f090187b
Michał Gutowski <michal.gutowski@westwing.pl>
2018-03-29 22:13:24 +0200

    Removed hardcoded document value

--- src/main/scala/net/cucumbersome/sentenceGenerator/Main.scala
+++ src/main/scala/net/cucumbersome/sentenceGenerator/Main.scala
@@ -10,25 +10,24 @@ import net.cucumbersome.sentenceGenerator.wordCounter.SentenceWordCounter
 import net.cucumbersome.sentenceGenerator.wordGenerator.NonEmptyNextWordGenerator
 import org.http4s.server.blaze.BlazeBuilder
 import org.http4s.server.middleware._
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.io.Source
-import scala.concurrent.duration._
+
 object Main extends StreamApp[IO] {
-
-  private val file = Source.fromFile("/Users/michal/Documents/teksty.txt")
-    .getLines().toList
-
-
-  private val sentences = FromStringTokenizer.readFromString(file.mkString("\n"))
-  private val wordCounts = SentenceWordCounter.countWords(sentences)
-  private val vector = NonEmptyVector(wordCounts.head, wordCounts.toVector)
-
-  private val generator = new SimpleSentenceGenerator(new NonEmptyNextWordGenerator(vector))
-
-  private val httpService = CORS(new SimpleSentenceGeneratorWebService(generator).service)
-
-
   override def stream(args: List[String], requestShutdown: IO[Unit]): fs2.Stream[IO, StreamApp.ExitCode] = {
+    val file = Source.fromFile(args.head)
+      .getLines().toList
+
+
+    val sentences = FromStringTokenizer.readFromString(file.mkString("\n"))
+    val wordCounts = SentenceWordCounter.countWords(sentences)
+    val vector = NonEmptyVector(wordCounts.head, wordCounts.toVector)
+
+    val generator = new SimpleSentenceGenerator(new NonEmptyNextWordGenerator(vector))
+
+    val httpService = CORS(new SimpleSentenceGeneratorWebService(generator).service)
+
     BlazeBuilder[IO]
       .bindHttp(8080, "0.0.0.0")
       .mountService(httpService, "/")
