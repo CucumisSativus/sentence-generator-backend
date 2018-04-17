3e530fc0d372e63189fc40948cb56f914682524a
Michał Gutowski <michal.gutowski@westwing.pl>
2018-04-17 21:14:12 +0200

    Updated separators

--- src/main/scala/net/cucumbersome/sentenceGenerator/tokenizer/FromStringTokenizer.scala
+++ src/main/scala/net/cucumbersome/sentenceGenerator/tokenizer/FromStringTokenizer.scala
@@ -3,6 +3,8 @@ package net.cucumbersome.sentenceGenerator.tokenizer
 import net.cucumbersome.sentenceGenerator.domain.{Sentence, Word}
 
 object FromStringTokenizer {
+  private val separators = Array(' ', '-')
+
   def readFromString(input: String): List[Sentence] = {
     val sentences = input.split('.')
     sentences.map(readSentence).filter(_.words.nonEmpty).toList
@@ -10,7 +12,7 @@ object FromStringTokenizer {
 
   def readSentence(sentence: String): Sentence = {
     Sentence.fromSeq(
-      sentence.split(' ').map(stringToWord).toSeq.filter(_.value.nonEmpty)
+      sentence.split(separators).map(stringToWord).toSeq.filter(_.value.nonEmpty)
     )
   }
 
